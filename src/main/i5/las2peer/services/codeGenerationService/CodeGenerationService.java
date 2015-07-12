32fcc6f3ee67d3dcdfc586ef133832834ede584b
PedeLa <lange@dbis.rwth-aachen.de>
2015-07-12 18:46:09 +0200

    Added first GitHub tests

--- src/main/i5/las2peer/services/codeGenerationService/CodeGenerationService.java
+++ src/main/i5/las2peer/services/codeGenerationService/CodeGenerationService.java
@@ -1,5 +1,12 @@
 package i5.las2peer.services.codeGenerationService;
 
+import java.io.IOException;
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import i5.cae.simpleModel.SimpleModel;
 import i5.las2peer.api.Service;
 
 /**
@@ -10,17 +17,32 @@ import i5.las2peer.api.Service;
  */
 public class CodeGenerationService extends Service {
 
+  public String gitHubUser;
+  public String gitHubPassword;
+
   public CodeGenerationService() {
     // read and set properties values
     setFieldValues();
+    GitHubClient client = new GitHubClient();
+    client.setCredentials(gitHubUser, gitHubPassword);
+    RepositoryService service = new RepositoryService();
+    try {
+      // prints out the name of the users repositories
+      for (Repository repo : service.getRepositories(gitHubUser))
+        System.out.println(repo.getName());
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   /**
+   * @param model
    * 
    * @return a string
    * 
    */
-  public String updateModel() {
+  public String createSource(SimpleModel model) {
     return null;
   }
 
