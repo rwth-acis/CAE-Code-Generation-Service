8abf9c68e53d7de84d08f7a61e1f3175ca0d0b9c
PedeLa <lange@dbis.rwth-aachen.de>
2015-07-13 10:39:49 +0200

    reorganized start scripts

---
+++ bin/join_network.bat
@@ -0,0 +1,13 @@
+:: this script starts a las2peer node providing the code-generation-service
+:: pls execute it from the bin folder of your deployment by double-clicking on it
+:: since this service only works in combination with the model persistence service,
+:: it tries to connect to a running node (which runs this persistence service)
+
+%~d0
+cd %~p0
+cd ..
+set BASE=%CD%
+set CLASSPATH="%BASE%/lib/*;"
+
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9012 -b 192.168.178.22:9011 startService('i5.las2peer.services.codeGenerationService.CodeGenerationService') interactive
+pause
---
+++ bin/join_network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this script starts a las2peer node providing the code-generation-service
+# pls execute it from the root folder of your deployment, e. g. ./bin/start_network.sh
+# since this service only works in combination with the model persistence service,
+# it tries to connect to a running node (which runs this persistence service)
+
+java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9012 -b 192.168.178.22:9011 startService\(\'i5.las2peer.services.codeGenerationService.CodeGenerationService\') interactive
