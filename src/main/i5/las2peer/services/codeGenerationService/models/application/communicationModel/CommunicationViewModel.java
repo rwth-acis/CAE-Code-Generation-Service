c8a53be4eccabc697b3a7a14c0ac36e0515ea3e7
PedeLa <lange@dbis.rwth-aachen.de>
2015-08-28 21:59:18 +0200

    added communication view model elements

---
+++ src/main/i5/las2peer/services/codeGenerationService/models/application/communicationModel/CommunicationViewModel.java
@@ -0,0 +1,12 @@
+package i5.las2peer.services.codeGenerationService.models.application.communicationModel;
+
+/**
+ * 
+ * A special view on an
+ * {@link i5.las2peer.services.codeGenerationService.models.application.Application}. Focuses on
+ * representing the communication between its different components.
+ * 
+ */
+public class CommunicationViewModel {
+
+}
