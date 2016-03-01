4b6c453025a255c8aa9c32e6005fa05c0ffb6006
Peter de Lange <lange@dbis.rwth-aachen.de>
2016-03-01 22:27:07 +0100

    updated start scripts

--- bin/join_network.bat
+++ bin/join_network.bat
@@ -9,5 +9,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9012 -b 192.168.178.22:9011 startService('i5.las2peer.services.codeGenerationService.CodeGenerationService') interactive
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9012 -b 192.168.178.22:9011 startService('i5.las2peer.services.codeGenerationService.CodeGenerationService@0.1') interactive
 pause
