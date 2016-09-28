11690ab4c45ab569ed9c4d3024b0ad4edd3603bc
Jonas Könning <jonas.koenning@gmail.com>
2016-09-28 17:05:02 +0200

    Path changes for GitLab support. Method for repo creation in GitLab

--- src/main/adapters/GitLabAdapter.java
+++ src/main/adapters/GitLabAdapter.java
@@ -13,7 +13,8 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class GitLabAdapter {
-	private final static String token = "3vj6hao1PZEHbWxuUd1p";
+	// TODO: Add token
+	private final static String token = "";
 	private final static String baseURL = "http://ginkgo.informatik.rwth-aachen.de:4080/api/v3/";
 
 	private static String getString(String url) {
@@ -86,6 +87,41 @@ public class GitLabAdapter {
 		return success;
 	}
 	
+	private static boolean createResource(String url, JSONObject data) {
+		HttpURLConnection c = null;
+		boolean success = false;
+		
+		try {
+			String body = data.toJSONString();
+			URL u = new URL(url);
+			c = (HttpURLConnection) u.openConnection();
+			c.setRequestMethod("POST");
+			c.setRequestProperty("PRIVATE-TOKEN", token);
+			c.setRequestProperty("Content-Type", "application/json");
+			c.setDoInput(true);
+		    c.setDoOutput(true);
+			c.setUseCaches(false);
+			c.connect();
+			int status = c.getResponseCode();
+			
+			switch (status) {
+			case 201:
+				success = true;
+				break;
+			}
+			
+		} catch (MalformedURLException e) {
+			// TODO: handle exception
+		} catch (IOException e) {
+			// TODO: handle exception
+		} finally {
+			if (c != null) {
+				c.disconnect();
+			}
+		}
+		return success;
+	}
+	
 	private static JSONObject getJSONObject(String url) {
 		String data = getString(url);
 		JSONParser parser = new JSONParser();
@@ -133,6 +169,12 @@ public class GitLabAdapter {
 			}
 		}
 		return success;
+	}
+	
+	public static boolean createRepo(String name, String description) {
+		boolean success = createResource(baseURL + "projects");
+		
+		return false;
 	}
 	
 }
