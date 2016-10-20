0aa541169bd4a5508afe07d4d7a366a3e629d8e3
Jonas Könning <jonas.koenning@gmail.com>
2016-10-20 12:06:38 +0200

    Preparing better exception handling

---
+++ src/main/i5/las2peer/services/codeGenerationService/exception/CodeGenerationException.java
@@ -0,0 +1,13 @@
+package i5.las2peer.services.codeGenerationService.exception;
+
+public class CodeGenerationException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2673252367883188023L;
+	public CodeGenerationException(String message) {
+		super(message);
+	}
+
+}
