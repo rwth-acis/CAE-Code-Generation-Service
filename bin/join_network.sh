4fa8fb5b1bcc0ae63d80ff5dd2c0189fa71c1f0c
ukimiawz <melisa.cecilia.mail@gmail.com>
2018-01-17 19:48:44 +0100

    Remove local files

--- bin/join_network.sh
+++ bin/join_network.sh
@@ -5,6 +5,4 @@
 # it tries to connect to a nd node (which runs this persistence service)
 #-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5432
 
-#java  -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9012 -b 134.61.69.177:9011 --service-directory service startService\(\'i5.las2peer.services.codeGenerationService.CodeGenerationService@0.1\'\) interactive
-#java  -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9012 -b 134.61.131.178:9011 --service-directory service startService\(\'i5.las2peer.services.codeGenerationService.CodeGenerationService@0.1\'\) interactive
-java  -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9012 -b 192.168.178.26:9011 --service-directory service startService\(\'i5.las2peer.services.codeGenerationService.CodeGenerationService@0.1\'\) interactive
+java  -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9012 -b 134.61.69.177:9011 --service-directory service startService\(\'i5.las2peer.services.codeGenerationService.CodeGenerationService@0.1\'\) interactive
